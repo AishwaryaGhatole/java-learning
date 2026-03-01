dae400672338a1cfcb433bdfb6e1133c53c7a4db
Aishwarya Ghatole <aishwaryaghatole8@gmail.com>
2026-03-01 22:05:27 +0530

    Prime Number Program (bug-fix)

--- Challange programs/src/PrimeNumber.java
+++ Challange programs/src/PrimeNumber.java
@@ -1,22 +1,21 @@
-
 import java.util.Scanner;
 
 public class PrimeNumber {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
-    System.out.print("Enter your Number : ");
     int num = input.nextInt();
-    boolean isPrime = primeNumberChecker(num);
+    boolean isPrime = isPrime(num);
+
     if(isPrime){
-      System.out.println("Given number is Prime");
-    }else{
-      System.out.println("Given number is not Prime");
+      System.out.print("The Given Number " + num + " is Prime.");
+    }else {
+      System.out.print("The Given Number " + num + " is non Prime.");
     }
 
   }
-  public static boolean primeNumberChecker(int num){
-    for(int i = 2; i <= num; i++){
-      if(num % i == 0){
+  public static boolean isPrime(int num){
+    for(int i = 2; i < num; i++){
+      if(num % i == 0 ){
         return false;
       }
     }
